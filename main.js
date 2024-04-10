73e2f44407ef15c32309ab122d7c4af947a00b93
jusky <measyolo@gmail.com>
2024-04-09 23:18:43 -0500

    Added all combinations

--- main.js
+++ main.js
@@ -9,13 +9,15 @@ const shoppingCart = document.querySelector('.navbar-shopping-cart')
 const shoppingCartPreview = document.querySelector('.product-detail')
 const cardsContainer = document.querySelector('.cards-container')
 // Aside2 preview
+const productAsideContainer = document.querySelector('.product-detail-secondary')
+const productAsideCloser = document.querySelector('.product-detail-close-secondary')
 
 
 
 navEmail.addEventListener('click', toogleMenu);
 burgerMenu.addEventListener('click', toogleBurger);
 shoppingCart.addEventListener('click', tooglePreview);
-
+productAsideCloser.addEventListener('click', closeProductAside)
 
 
 function toogleMenu(){
@@ -28,24 +30,41 @@ function toogleMenu(){
 
 function toogleBurger(){
     const isCartPreviewClosed = shoppingCartPreview.classList.contains('inactive-cart-preview')
+    const isAsideClosed = productAsideContainer.classList.contains('inactive')
     if (!isCartPreviewClosed){
         shoppingCartPreview.classList.toggle('inactive-cart-preview')
     }
-    
+    if (!isAsideClosed){
+        productAsideContainer.classList.add('inactive')
+    }
     mobileMenu.classList.toggle('activate-burger')
 }
 
 function tooglePreview(){
     const isBurgerClosed = mobileMenu.classList.contains('activate-burger')
-    const isDesktopMenuClosed = mobileMenu.classList.contains('inactive')
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive')
+    const isAsideClosed = productAsideContainer.classList.contains('inactive')
     if (isBurgerClosed){
         mobileMenu.classList.toggle('activate-burger')
     }
     if (!isDesktopMenuClosed) {
         desktopMenu.classList.add('inactive')
     }
+    if (!isAsideClosed) {
+        productAsideContainer.classList.add('inactive')
+    }
     shoppingCartPreview.classList.toggle('inactive-cart-preview')
+}
 
+function openProductAside (){
+    productAsideContainer.classList.toggle('inactive')
+}
+function closeProductAside (){
+    const isCartPreviewClosed = shoppingCartPreview.classList.contains('inactive-cart-preview')
+    if (!isCartPreviewClosed){
+        shoppingCartPreview.classList.add('inactive-cart-preview')
+    }
+    productAsideContainer.classList.add('inactive')
 }
 
 
@@ -72,6 +91,8 @@ for (product of productList) { // eeste for of itera los "products" del array pr
 
     const productImg = document.createElement('img');
     productImg.setAttribute('src', product.image)
+    productImg.addEventListener('click', openProductAside)
+    
 
     const productInfo= document.createElement('div');
     productInfo.classList.add('product-info')
